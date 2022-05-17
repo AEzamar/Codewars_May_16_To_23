5a9f85127299691924852a7a26ce4a78ceec0fde
AEzamar <60367320+AEzamar@users.noreply.github.com>
2022-05-16 19:57:39 -0500

    Solving dontGiveMeFive codewars kata

---
+++ app.js
@@ -0,0 +1,16 @@
+"use strict";
+//PREP
+//Parameters: 
+//2 Numbers will be passed
+//The first number will always be smaller than the second
+//Both numbers might be negative
+
+//Result:
+//The function should return the total amount of numbers between the first and second number
+//Numbers that contain 5 should not be included
+//The starting and ending numbers have to be counted
+
+//Examples:
+//dontGiveMeFive(1, 9); //8
+//dontGiveMeFive(4, 17); //12 
+
