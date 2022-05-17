7072bcd41e4e0e266e8982b47adddcc9f6783e10
AEzamar <60367320+AEzamar@users.noreply.github.com>
2022-05-16 21:01:10 -0500

    Update app.js

--- app.js
+++ app.js
@@ -12,5 +12,23 @@
 
 //Examples:
 //dontGiveMeFive(1, 9); //8
-//dontGiveMeFive(4, 17); //12 
+//dontGiveMeFive(4, 17); //12
 
+//Pseudo-Code:
+//Loop from start to end
+//Filter numbers that have the number 5 on them
+//Return length of array of filtered numbers 
+
+function dontGiveMeFive(start, end) {
+    const numArr = [];
+    for(let i = Math.abs(start); i <= Math.abs(end); i++) {
+        numArr.push(String(i));
+    }
+    return numArr.filter(num => !num.startsWith("5"));
+}
+
+console.log(dontGiveMeFive(1, 9));
+console.log(dontGiveMeFive(4, 17));
+console.log(dontGiveMeFive(-2, -13));
+
+console.log("15".endsWith("5"));
