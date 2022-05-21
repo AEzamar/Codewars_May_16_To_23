347b86918bf952be9a7be9af01bd5a69d711d559
AEzamar <60367320+AEzamar@users.noreply.github.com>
2022-05-21 12:52:01 -0500

    Update plusOneArray.js

---
+++ plusOneArray.js
@@ -0,0 +1,46 @@
+//PREP
+//Parameters:
+//Array has to have integers
+//The integers in the array can only be positive and single digit
+
+//Return:
+//Invalid inputs should return null
+//The function should return an array with the number the array represents + 1
+
+//Examples:
+//[3, 4, 5] should be taken as 345 so the function should return 345 + 1 = [3, 4, 6]
+//[4, 3, 2, 5] 4325 + 1 = [4, 3, 2, 6];
+
+//Pseudo-Code:
+//Iterate over array
+//Check if array.length > 0, check that array has no negative integers, check that integers are single digit(0 - 9);
+//If array passes all the conditions reduce arrays integers to a single number
+//Add 1 to the single number
+//Split single integer into array of integers
+//Return new array of integers
+
+function upArray(arr) {
+    if(arr.length > 0 && arr.every(int => int >= 0 && int <= 9)) {
+        const singleIntStr = arr.join("");
+        const singleInt = Number(singleIntStr) + 1;
+        const stringedSingleInt = String(singleInt);
+        return Array.from(stringedSingleInt, strInt => Number(strInt));
+    }else {
+        return null;
+    }
+}
+
+console.log(upArray([2, 3, 9]));
+console.log(upArray([4, 3, 2, 5]));
+console.log(upArray([]));
+
+
+/* function upArray1(arr) {
+    if(arr.length > 0 && arr.every(int => int >= 0 && int <= 9)) {
+        const singleIntStr = arr.join("");  
+        return Array.from(singleIntStr, strInt => Number(strInt));
+    }else {
+        return null;
+    }
+}
+console.log(upArray1([2, 3, 9])); */
