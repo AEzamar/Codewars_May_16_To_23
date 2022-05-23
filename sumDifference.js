177d41653754883ec16415f6d31cddbb9e22bbd8
AEzamar <60367320+AEzamar@users.noreply.github.com>
2022-05-23 13:53:03 -0500

    Update sumDifference.js

--- sumDifference.js
+++ sumDifference.js
@@ -18,14 +18,47 @@
 //Return the total sum
 
 function sumOfDifferences(arr) {
-    const sortedArr = arr.sort((a, b) => b - a);
-    let curr = 0;
-    let next = 1;
     let difference;
-    for(let i = 0; i < sortedArr.length; i++) {
-        difference = arr[curr] - arr[next] + arr[curr + 1] - arr[next + 1];
-    }
-    return arr.length === 0 ? null : difference;
+    let length = arr.length;
+    console.log('Length:', length)
+    const sortedArr = arr.sort((a, b) => b - a);
+    let curr = 0, next = 1;
+        for(let i = 0; i <= sortedArr.length; i++) {
+            //console.log(arr[curr], arr[next], arr[curr + 1], arr[next + 1]);
+            difference = sortedArr[curr] - sortedArr[next] + sortedArr[curr + 1] - sortedArr[next + 1];
+        }
+    return length === 0 ? 0 : length === 1 ? 0 : difference;
 }
 
-console.log(sumOfDifferences([1, 2, 10]));
+/* console.log(sumOfDifferences([1, 2, 10]));
+console.log(sumOfDifferences([-3, -1, -2]));
+console.log(sumOfDifferences([-17, 17]));
+console.log(sumOfDifferences([])); */
+
+function sumOfDifferences1(arr) {
+    let difference;
+    let length = arr.length;
+    const sortedArr = arr.sort((a, b) => b - a);
+    let curr = 0, next = 1;   
+    if(length ===  2) {
+        difference = sortedArr[curr] - sortedArr[next];
+    }
+    if(length > 2) {
+        difference = sortedArr.reduce((accu, curr) => {
+            const result = accu + curr;
+            console.log(result);
+        }, 0);
+        /* for(let i = 0; i <= sortedArr.length; i++) {
+            difference = sortedArr[curr] - sortedArr[next] + sortedArr[curr++] - sortedArr[next++];
+            //next++;
+            //curr++;
+        } */
+    }
+    return length === 0 ? 0 : length === 1 ? 0 : difference;
+}
+
+console.log(sumOfDifferences1([1, 2, 10]));
+console.log(sumOfDifferences1([-3, -1, -2]));
+console.log(sumOfDifferences1([-17, 17]));
+console.log(sumOfDifferences1([]));
+console.log(sumOfDifferences1([5,11,13,6,0,-14,13]));
