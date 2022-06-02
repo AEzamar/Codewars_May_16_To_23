eacea67e544042d729f5ba0593db949f4f54781a
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-06-02 09:36:24 -0500

    Finished product array codewars kata

--- productArray.js
+++ productArray.js
@@ -2,7 +2,7 @@
 //Parameter:
 //An array of positive integers of at least length = 2
 
-//Return
+//Return:
 //An array of the result of multypling pairs of elements in the array
 //The elements have to be multiplied in pairs, the pairs are decided by excluding the number with the index that is equal to the number of the current operation
 //If we're on the first operation of the first element has to be excluded and the other 2 have to be multiplied and the result has to be pushed to a new array
@@ -22,41 +22,52 @@
 //After loop is over return prodArr with all the results of the multiplications
 
 //Making a helper function
-//Except index has to be i in the productArray function's for loop
+//exceptindex has to be i in the productArray function's for loop
+
 function extractAndReduce(array, excepIndex) {
-    const exceptArr = [];
-    const arrayCopy = [...array];
-    //const slicedArr = arrayCopy.slice(excepIndex, excepIndex + 1);
-    const filteredArr = arrayCopy.filter(ele => ele !== arrayCopy[excepIndex]);
-    return filteredArr.reduce((tot, cur) => tot * cur);
+    const slice1 = array.slice(0, excepIndex);
+    const slice2 = array.slice(excepIndex + 1);
+    const slicedArr = [...slice1, ...slice2];
+    return slicedArr.reduce((tot, cur) => tot * cur);
 }
 
-console.log(extractAndReduce([3, 27, 4, 2], 0));
-console.log(extractAndReduce([13,10,5,2,9], 1), );
+console.log(extractAndReduce([7, 31, 64, 8, 1, 4, 4, 24, 10], 4));
 
 /* function productArray(numbers) {
-    let opCount = 0;
-    let productArr = [];
-    const numbersCopy = [...numbers];
-    for(let i = 0; i < numbersCopy.length - 1; i++) {
-        if(opCount === i) {
-            numbersCopy.splice(i, 1);
-            console.log()
-            productArr.push(numbersCopy[i])
-            
-            //productArr.push(numbers[i])
-        }
-        opCount++;
+    const productArr = [];
+    if(numbers.length === 2) {
+        const [ele1, ele2] = numbers;
+        return [ele2, ele1];
     }
-    return productArr.reduce((tot, cur) => tot * cur);
+    for(let i = 0; i < numbers.length; i++) {
+        productArr.push(extractAndReduce(numbers, i));
+    }
+    return productArr;
 } */
 
-function productArray(number) {
-    const opCount = 0;
+function productArray(numbers){
     const productArr = [];
-    for(let i = 0; i < number.length; i++) {
-        productArr.push(extractAndReduce(number, i));
+    const [ele1, ele2] = numbers;
+    for(let i = 0; i < numbers.length; i++) {
+      productArr.push(extractAndReduce(numbers, i));
     }
-}
+    return numbers.length === 2 ? [ele2, ele1] : productArr;   
+  }
 
+console.log(productArray([20, 12]));
 console.log(productArray([3,27,4,2]));
+console.log(productArray([13,10,5,2,9]));
+console.log(productArray([7, 31, 64, 8, 1, 4, 4, 24, 10]));
+
+//Codewars solution:
+//Come on man, what is this?!
+/* function productArray(numbers){
+    return numbers.map(x => numbers.reduce((a,b) => a * b) / x)
+  } */
+
+//This is very similar to the last solution ^ but it's a little more readable 
+/*   function productArray(numbers)
+{
+  var sum = numbers.reduce((x, y) => x * y);
+  return numbers.map(x => sum / x);
+} */
