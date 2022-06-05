02949e3be8273c51f281be00164945dabf4ef41c
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-06-05 10:38:45 -0500

    Changed script to arrayLeaders.js to solve a codewars kata

--- arrayLeaders.js
+++ arrayLeaders.js
@@ -1,7 +1,30 @@
+//PREP
+//Parameters:
+//An array of positive and negative integers
+
+//Return:
+//An array that contains the integers that are leaders
+//An integer is a leader when it's bigger that the sum of all the elements to it's right
+
+//Example:
+//arrayLeaders([1,2,3,4,0]) => 1 is not bigger than 2 + 3 + 4 or 9 => 2 is not bigger than 7 => 3 is not bigger than 4 => 4 is bigger than 0 // the result is [4], being the only leader of this array
+//arrayLeaders([16,17,4,3,5,2]) => [17, 5, 2];
+//arrayLeaders([[-36,-12,-27]]) => [-36, -12];
+
 //Pseudo-code:
-//
-//Make variable to store single integer from array called possibleLeader = 0;
-//array[possibleLeader] This element will only go up in value if the element is not the leader
+//Create empty array to store leaders leadersArr
+//Loop through numbers passed in the function
+//Make variable to store single integer from numbers called possibleLeader = 0;
+//numbers[possibleLeader] This element will be changed manually to store each integer of the array individually to compare it with the elements to it's right
+//Slice a section of the array that contains the integers to the right of numbers[possibleLeader]
+//Reduce this sub-array with a sum
+//Compare numbers[possibleLeader] with the result of the reduction
+//if numbers[possibleLeader] is bigger than the result of the reduction, push possibleLeader to leadersArr
+//Repeat the steps until the end of the array is reached
+//If the last element of the array is bigger than 0 push it to leadersArr too
+//After all the iterations are done return leadersArr
+
+//array.slice(array[possibleLeader]);
 //array.every(num => {
 //  if(array[possibleLeader] > num) {
 //      return array[possibleLeader];   
@@ -9,3 +32,19 @@
     //possibleLeader++;
 //}
 //})
+
+function arrayLeaders(numbers) {
+    const leadersArr = [];
+    let possibleIndex = 0
+    const leftSide = numbers.slice(0, possibleIndex);
+    const rightSide = numbers.slice(possibleIndex + 1);
+    const subArr = [...leftSide, ...rightSide];
+    const subArrTotal = subArr.reduce((tot, cur) => tot + cur, 0);
+    for(let i = 0; i < numbers.length; i++) {
+        if(numbers[possibleIndex] > subArrTotal) leadersArr.push(numbers[possibleIndex]);
+        possibleIndex++
+    }
+    return rightNumbers;
+}
+
+console.log(arrayLeaders([1,2,3,4,0]));
