ac1c1d8b2f7888be9d4df7b0fbae0da020c2f8c5
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-06-20 12:29:38 -0500

    Solving a codewars kata to count how many specific characters are in a string

--- countRecurringChars.js
+++ countRecurringChars.js
@@ -10,3 +10,30 @@
 //count('aba') => { a: 1, b: 2}
 
 //Pseudo-code:
+//Make two variables one for keeping track of how many a's and b's are in the string, called aCount and bCount respectively
+//Loop through string
+//If a character in the string equals to a add 1 to aCount
+//if a character in the string equals to b add 1 tp bCount
+//Create object with keys a and b and the value of a key should be aCount, the value of b key should be bCount
+//Return object with keys a and b with values of aCount and bCount
+
+function count(string) {
+    let aCount = 0, bCount = 0;
+    let charObj = {
+        a: 0,
+        b: 0
+    }
+    for(let i = 0; i < string.length; i++) {
+        if(string[i] === 'a') {
+            aCount++;
+            charObj[a] = aCount;
+        }
+        if(string[i] === 'b') {
+            bCount++;
+            charObj[b] = bCount;
+        }
+    }
+    return charObj;
+}
+
+console.log(count('abba'));
