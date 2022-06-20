a792af909e1442ccb0032c97a09e92db9a451d44
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-06-20 08:06:32 -0500

    Started to solve unfinished kata to count the recurring characters in a string

---
+++ countRecurringChars.js
@@ -0,0 +1,12 @@
+//PREP
+//A string that may or may not contain recurring characters
+
+//Return:
+//An object with the recurring characters as keys and the amount of times
+//the characters appears in the string as the values
+//If the string that gets passed in the function is empty, return an empty object
+
+//Example:
+//count('aba') => { a: 1, b: 2}
+
+//Pseudo-code:
