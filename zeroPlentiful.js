ef7c6ee2fab5c974ef3d1fdc19897e31e443d792
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-06-26 00:53:15 -0500

    Modified zeroPlentiful.js

--- zeroPlentiful.js
+++ zeroPlentiful.js
@@ -108,6 +108,18 @@ function zeroPlentiful3(arr) {
   return sequenceCount; 
 }
 
-console.log(zeroPlentiful3([0, 0, 0, 4, 1, 2, 0, 0, 0, 0, 3, 1, 2, 0, 0, 0, 0, 0]));
+/* console.log(zeroPlentiful3([0, 0, 0, 4, 1, 2, 0, 0, 0, 0, 3, 1, 2, 0, 0, 0, 0, 0]));
 console.log(zeroPlentiful3([0, 0, 0, 0, 0]));
-console.log(zeroPlentiful3([0, 0, 0, 0, 1, 0, 0, 0, 0, 0]));
+console.log(zeroPlentiful3([0, 0, 0, 0, 1, 0, 0, 0, 0, 0])); */
+
+function zeroPlentiful4(arr) {
+  let zeroCount = 0, sequenceCount = 0;
+  for(let i = 0; i < arr.length; i++) {
+    if(arr[i] === 0) {
+      zeroCount++;
+    }else {
+      zeroCount = 0;
+    }
+  }
+  return zeroCount >= 4 ? sequenceCount++ : sequenceCount;
+}
