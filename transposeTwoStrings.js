03d8c856549810b4d84f3de336e7355589e96d52
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-06-27 23:27:17 -0500

    Created new script to solve kata about transposing strings

---
+++ transposeTwoStrings.js
@@ -0,0 +1,10 @@
+//PREP
+//Parameters:
+//An array with two strings
+
+//Return:
+//The function should return the string in a format were we display
+//the characters of each strin in a column, so we should have two columns each one
+//with every character of both strings
+
+//Examples:
