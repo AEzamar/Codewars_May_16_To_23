f44cfd5ee375711af9c366b54098e30d7ddf8ade
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-06-28 22:28:39 -0500

    Starting to solve tranpose two strings kata

--- transposeTwoStrings.js
+++ transposeTwoStrings.js
@@ -1,6 +1,6 @@
 //PREP
 //Parameters:
-//An array with two strings
+//An array with two strings as elements
 
 //Return:
 //The function should return the string in a format were we display
@@ -8,3 +8,23 @@
 //with every character of both strings
 
 //Examples:
+//transposeTwoStrings(['Hello','World']) =>
+//H W
+//e o
+//l r
+//l l
+//o d
+
+//Pseudo-code:
+//Loop through string 0 and 1 of array
+//Print each letter of each string and start a new line
+//Keep printing characters until the string's length equals -1
+//If one string is longer than the other replace undefined characters with whitespace
+
+function transposeTwoStrings(array) {
+    let transposeStr = "";
+    for(let i = 0; i < array[0].length; i++) {
+        transposeStr += array[0][i];
+    }
+    return transposeStr;
+}
