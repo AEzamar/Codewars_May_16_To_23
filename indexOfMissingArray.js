eb59729015c0030636201f90e06b3c2d01a9d9c7
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-07-13 19:37:25 -0500

    Testing indexOfMissingArray.js

--- indexOfMissingArray.js
+++ indexOfMissingArray.js
@@ -36,3 +36,4 @@ function getLengthOfMissingArray(array) {
 
 console.log(getLengthOfMissingArray([[ 1, 2 ], [ 4, 5, 1, 1 ], [ 1 ], [ 5, 6, 7, 8, 9 ]]));
 console.log(getLengthOfMissingArray([[2, 3, 1, 2], [1], [1, 0, 9], [2, 1, 9, 9, 1]]));
+console.log(getLengthOfMissingArray([[4, 1, 2, 3, 4, 5], [1, 2, 3], [0, 1], [0, 1, 1, 4]]))
